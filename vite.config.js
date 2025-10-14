56c79329785f3db986a6b6a0d29a10112a297226
FernandoLion <fernandoshgz@gmail.com>
2025-10-14 17:51:29 -0500

    Ajuste final para despliegue en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/ApiSimpsons_2025IIg1_Leon_Jhojan_Ramirez_Javier/', 
