f0095fa53ae8bda4eeb3968736910e53dbb6921c
FernandoLion <fernandoshgz@gmail.com>
2025-10-14 17:59:12 -0500

    pruebaaaa

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ApiSimpsons_2025IIg1_Leon_Jhojan_Ramirez_Javier/', 
+  base: '/ApiSimpons_2025IIg1_Leon_Jhojan_Ramirez_Javier/'
 })
